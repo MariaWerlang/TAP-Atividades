94ab2897a1969dbd4b831870abbcd097b1a5090a
MariaWerlang <maria.werlang@etec.sp.gov.br>
2024-04-19 20:36:05 -0300

    Atividade 19-04

---
+++ RPG/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use App\Mago;
+use App\Guerreiro;
+use App\Arqueiro;
+
+$mago = new Mago();
+$guerreiro = new Guerreiro();
+$arqueiro = new Arqueiro();
+
+$criatura->Atacar("Chama Suprema", "175");
+$titã->Atacar("Golpe Trovejante", "200");
+$criatura->Atacar("Seta Astral", "150");
+
+?>
